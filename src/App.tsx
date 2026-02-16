0497da700c8dc958e532001c5c56f4fb56e702a2
Justin Li <justin031607@gmail.com>
2026-02-15 19:37:00 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <div className="panel left-panel">
+        neural network
+      </div>
+      <div className="panel right-panel">
+        game
+      </div>
+    </div>
+  )
+}
+
+export default App
